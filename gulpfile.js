16e0947790cb8f025686a1bcd3bff299322fce5c
Paul Lauer <pavel@lauer.com.ua>
2017-07-11 01:00:26 +0300

    start dev

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,12 @@ var gulp = require('gulp'),
     pug = require('gulp-pug'),
     gulpPugBeautify = require('gulp-pug-beautify'),
     sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
     include = require("gulp-include");
- 
+
 
 var source = '_application/',
     dest = 'build/',
@@ -27,10 +31,11 @@ var source = '_application/',
             }
         },
         js: {
-            in: source + 'js/*.js',
+            in: source + 'js/**/*.*',
             out: dest + 'js/',
             jquery_bower_src: 'bower_components/jquery/dist/jquery.min.js',
-            bootstrap_js_src: bootstrapSass.in + 'dist/js/bootstrap.min.js'
+            bootstrap_js_src: bootstrapSass.in + 'dist/js/bootstrap.min.js',
+            tether_src: 'bower_components/tether/dist/js/tether.min.js'
         },
         img: {
             in: source + 'images/**/*.*',
@@ -38,13 +43,20 @@ var source = '_application/',
         },
         fonts: {
             in: source + 'fonts/**/*.*',
-            out: dest + 'fonts/'
+            out: dest + 'fonts/',
+            font_awesome_css: 'bower_components/components-font-awesome/css/font-awesome.min.css',
+            font_awesome_map: 'bower_components/components-font-awesome/css/font-awesome.css.map',
+            font_awesome_fonts: 'bower_components/components-font-awesome/fonts/**/*.*',
+            font_awesome_CSSout: dest + 'styles/libs/FontAwesome/',
+            font_awesome_out: dest + 'fonts/FontAwesome/'
         },
         watch: {
             pug: source+'template/**/*.pug',
-            js: source+'js/**/*.js',
+            js: source+'js/**/*.*',
             css: source + 'scss/**/*',
-            bootstrapCSS: source + 'scss/**/*'
+            bootstrapCSS: source + 'scss/**/*',
+            fonts: source + 'fonts/**/*',
+            images: source + 'images/**/*.*'
         }
     }
 
@@ -55,16 +67,18 @@ var source = '_application/',
 
 // SCSS
 gulp.task('sass', ['fonts'], function () {
-    console.log("-- SCSS --");
+    // console.log("-- SCSS --");
     return gulp.src(path.css.in)
         .pipe(sass(path.css.sassOpts))
         .pipe(sourcemaps.init({loadMaps: true}))
         .pipe(sourcemaps.write())
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest(path.css.out));
 });
 // JADE (PUG)
 gulp.task('pug', function () {
-    console.log("-- JADE (PUG) --");
+    // console.log("-- JADE (PUG) --");
     return gulp.src(path.pug.compile)
         .pipe(gulpPugBeautify({omit_empty: true}))
         .pipe(pug({pretty: true}))
@@ -72,15 +86,15 @@ gulp.task('pug', function () {
 });
 // ECMA SCRIPT (JS)
 gulp.task("scripts", function() {
-  console.log("-- ECMA SCRIPT (JS) --");
-  return gulp.src([path.js.jquery_bower_src,  path.js.bootstrap_js_src, path.js.in])
+  // console.log("-- ECMA SCRIPT (JS) --");
+  return gulp.src([path.js.jquery_bower_src, path.js.bootstrap_js_src, path.js.in, path.js.tether_src])
     .pipe(include())
     .on('error', console.log)
     .pipe(gulp.dest(path.js.out));
 });
 // IMAGES COPY
 gulp.task('images', function () {
-    console.log("-- IMAGES COPY --");
+    // console.log("-- IMAGES COPY --");
     gulp.src(path.img.in)
         .pipe(imagemin({
             progressive: true,
@@ -89,21 +103,33 @@ gulp.task('images', function () {
             interlaced: true
         }))
         .pipe(gulp.dest(path.img.out)) //И бросим в build
-        .pipe(reload({stream: true}));
 });
 // FONTS COPY
-gulp.task('fonts', function() {
-    console.log("-- FONTS COPY --");
+gulp.task('awesome', function() {
+    // console.log("-- FontAwesome COPY --");
+    gulp.src(path.fonts.font_awesome_fonts)
+    .pipe(gulp.dest(path.fonts.font_awesome_out))
+});
+gulp.task('awesomeCSS', function() {
+    // console.log("-- FontAwesome COPY --");
+    gulp.src([path.fonts.font_awesome_css, path.fonts.font_awesome_map])
+    .pipe(gulp.dest(path.fonts.font_awesome_CSSout))
+});
+
+gulp.task('fonts', ['awesome', 'awesomeCSS'], function() {
+    // console.log("-- FONTS COPY --");
     gulp.src(path.fonts.in)
-        .pipe(gulp.dest(path.fonts.out))
+    .pipe(gulp.dest(path.fonts.out))
 });
 
 
 // default task
-gulp.task('default', ['sass', 'scripts', 'pug'], function () {
-    console.log("-- WATCH --");
+gulp.task('default', ['sass', 'scripts', 'pug', 'fonts', 'images'], function () {
      gulp.watch(path.watch.css, ['sass']);
      gulp.watch(path.watch.bootstrapCSS, ['sass']);
-     gulp.watch(path.watch.pug, ['pug']); 
-     gulp.watch(path.watch.js, ['scripts']); 
+     gulp.watch(path.watch.pug, ['pug']);
+     gulp.watch(path.watch.js, ['scripts']);
+     gulp.watch(path.watch.fonts, ['fonts']);
+     gulp.watch(path.watch.images, ['images']);
+    console.log("-- WATCH FINISH --");
 });
